01bf9a540b2bc65004a0b82f589409aaa318fff7
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-26 17:41:29 -0700

    inputBox object null

--- todo-app.js
+++ todo-app.js
@@ -1,5 +1,51 @@
-//STEP 1: Create a user input.
-// don't use this: todo = window.prompt("Add a todo: ");
+
+const todos = [ "dishes", "laundry", "take over the world" ];
+
+//WORKED:
+// function createList() {
+//     const ul = document.getElementById("list");
+//     const li = document.createElement("li");
+//     //just for fun:
+//     //li.textContent = "Peralta, you are an amazing detective slash genius";
+//     li.textContent = todos[2];
+//     ul.appendChild(li);
+// }
+
+//DOESN'T WORK YET (console error lists 'inputBox' in if statement as null):
+function addTask()   {
+    const inputBox = document.getElementById("inputBox");
+    //if (inputBox.value === " ") {
+    //maybe this is simpler:    
+    if (!inputBox.value) {
+        alert("Please enter a todo!");
+    }
+        else if (inputBox.value) and (KeyboardEvent.code === 13); {
+            const ul = document.getElementById("list");
+            const li = document.createElement("li");
+            ul.appendChild(li);
+        }
+}
+
+window.onload = addTask();
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     inputBox.addEventListener("inputBox", addTask());  
+// });
+
+
+// inputBox.addEventListener("inputBox", (event) =>   {
+//     if (event.which === 13) {
+//     addTask();
+//     } 
+// });
+
+
+
+
+//PROJECT PLAN:
+
+//STEP 1: Create a user input field which accepts user's value.
+//*Don't use this: todo = window.prompt("Add a todo: ");
 //*Check functionality using: let inputValue = document.getElementById("enter").value;
 
 //STEP 2: Create a button that when clicked will submit user input to UL.
@@ -14,36 +60,18 @@
 //*Check button functionality using console.log.
 
 
+
 //NOTES:
 // - You will need to use addEventlisteners for add and delete buttons. 
-// - learn how to create <li> by using: document.createElement('li');
+// - Learn how to create <li> by using: document.createElement('li');
 
 
 
+//CODE SNIPPETS YOU MIGHT NEED:
 //var mkList = document.createElement("li");
 //mkList.innerHTML = ;
 
-const inputBox = document.querySelector(".inputBox");
+//const inputBox = document.getElementById(".inputBox");
 //const addBtn = document.querySelector("#addBtn");
-
-
-function addTask()   {
-    if (addInput.value === " ") {
-        alert("Stop messin' around and enter a todo!");
-    }
-    else    {
-    const ul = document.querySelector("listholder");
-    const li = document.createElement("li");
-    li.innerHTML(inputBox.value);
-    ul.appendChild(li);
-    }
-}
-
-//keyup isn't an id or class in my HTML so produces error in console.
-inputBox.addEventListener("keyup", (event) =>   {
-    if (event.which === 13) {
-    addTask();
-    } 
-});
 
 //let inputValue = document.getElementById("enter").value;
