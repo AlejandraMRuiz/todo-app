c4c54084de97ca448150d8617077998ccad68025
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-29 22:33:25 -0700

    dblclick works but not with click and vice versa

--- todo-app.js
+++ todo-app.js
@@ -11,9 +11,16 @@ function renderTask(keyboardEvent)   {
         ul.appendChild(li);
         li.textContent = text;
         console.log("text value is: ", text);
-        li.addEventListener("click", function()    {
-            console.log("you clicked me");
-            li.innerHTML = text.strike();
+        // li.addEventListener("click", function()    {
+        //     console.log("you clicked me");
+        //     li.innerHTML = text.strike();
+        // });
+
+        //below code WORKS, but not in tandem with "click" event.
+        //See: https://www.w3schools.com/jquery/event_dblclick.asp
+        li.addEventListener("dblclick", function()  {
+            console.log("you double-clicked me");
+            li.innerHTML = "";
         });
     }
 }
@@ -27,6 +34,8 @@ function deleteList() {
 } 
 
 deleteListBtn.addEventListener("click", deleteList);
+
+
 
 
 
@@ -63,10 +72,14 @@ deleteListBtn.addEventListener("click", deleteList);
 //STEP 6: Create a button that individually deletes tasks that are strikethrough.
 //*Check button functionality using console.log.
 
-//STEP 7: Upgrade style.
+//STEP 7: Ensure that when user clicks task again, the strikethrough comes off it
+//in case they accidentally marked a task as complete.
+//Check it. 
+
+//STEP 8: Upgrade style.
 //Check it.
 
-//STEP 8: Clean & refactor code.
+//STEP 9: Clean & refactor code, including changing out any "innerHTMLs" for alternative.
 //Check it.
 
 
@@ -89,3 +102,31 @@ deleteListBtn.addEventListener("click", deleteList);
 
 // var list = document.getElementById("myList");   // Get the <ul> element with id="myList"
 //            // Remove <ul>'s first child node (index 0)
+
+
+//USE THIS TO GET CLICK & DBLCLICK WORKING IN TANDEM:
+// var the_div = document.getElementById('the_div');
+
+// function singleClick() {
+//     the_div.style.backgroundColor = '#0F0';
+// }
+
+// function doubleClick() {
+//     the_div.style.backgroundColor = '#00F';
+// }
+
+// var clickCount = 0;
+
+// the_div.addEventListener('click', function() {
+//     clickCount++;
+//     if (clickCount === 1) {
+//         singleClickTimer = setTimeout(function() {
+//             clickCount = 0;
+//             singleClick();
+//         }, 400);
+//     } else if (clickCount === 2) {
+//         clearTimeout(singleClickTimer);
+//         clickCount = 0;
+//         doubleClick();
+//     }
+// }, false);
