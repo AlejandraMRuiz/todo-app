4fd522d336e6ab3d808a0bd11d6e42d4350656b9
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-30 12:06:53 -0700

    click and dblclick on same element work in tandem

--- todo-app.js
+++ todo-app.js
@@ -11,17 +11,32 @@ function renderTask(keyboardEvent)   {
         ul.appendChild(li);
         li.textContent = text;
         console.log("text value is: ", text);
-        // li.addEventListener("click", function()    {
-        //     console.log("you clicked me");
-        //     li.innerHTML = text.strike();
-        // });
 
-        //below code WORKS, but not in tandem with "click" event.
-        //See: https://www.w3schools.com/jquery/event_dblclick.asp
-        li.addEventListener("dblclick", function()  {
+        function singleClick() {
+            console.log("you clicked me");
+            li.innerHTML = text.strike();
+        }
+
+        function doubleClick() {
             console.log("you double-clicked me");
             li.innerHTML = "";
-        });
+        }
+        
+        var clickCount = 0;
+        
+        li.addEventListener("click", function() {
+            clickCount++;
+            if (clickCount === 1) {
+                singleClickTimer = setTimeout(function() {
+                clickCount = 0;
+                singleClick();
+                }, 400);
+            } else if (clickCount === 2) {
+                clearTimeout(singleClickTimer);
+                clickCount = 0;
+                doubleClick();
+            }
+        }, false);
     }
 }
 
@@ -66,11 +81,12 @@ deleteListBtn.addEventListener("click", deleteList);
 //Check it.
 //DONE!!!
 
-//STEP 5: Fix input field so it clears after hitting enter vs. user doing so manually.
-//Check it.
-
-//STEP 6: Create a button that individually deletes tasks that are strikethrough.
+//STEP 5: Create a button that individually deletes tasks that are strikethrough.
 //*Check button functionality using console.log.
+//DONE!!!
+
+//STEP 6: Fix input field so it clears after hitting enter vs. user doing so manually.
+//Check it.
 
 //STEP 7: Ensure that when user clicks task again, the strikethrough comes off it
 //in case they accidentally marked a task as complete.
@@ -83,50 +99,5 @@ deleteListBtn.addEventListener("click", deleteList);
 //Check it.
 
 
-//NOTES:
-// - You will need to use addEventlisteners for add and delete buttons. 
 
 
-//CODE SNIPPETS YOU MIGHT NEED:
-//var mkList = document.createElement("li");
-//mkList.innerHTML = ;
-
-//const inputBox = document.getElementById(".inputBox");
-//const addBtn = document.querySelector("#addBtn");
-
-// function deleteList()   {
-//     list.removeChild(list.childNodes[0]);
-// }
-
-// deleteListBtn.addEventListener("click", deleteList);
-
-// var list = document.getElementById("myList");   // Get the <ul> element with id="myList"
-//            // Remove <ul>'s first child node (index 0)
-
-
-//USE THIS TO GET CLICK & DBLCLICK WORKING IN TANDEM:
-// var the_div = document.getElementById('the_div');
-
-// function singleClick() {
-//     the_div.style.backgroundColor = '#0F0';
-// }
-
-// function doubleClick() {
-//     the_div.style.backgroundColor = '#00F';
-// }
-
-// var clickCount = 0;
-
-// the_div.addEventListener('click', function() {
-//     clickCount++;
-//     if (clickCount === 1) {
-//         singleClickTimer = setTimeout(function() {
-//             clickCount = 0;
-//             singleClick();
-//         }, 400);
-//     } else if (clickCount === 2) {
-//         clearTimeout(singleClickTimer);
-//         clickCount = 0;
-//         doubleClick();
-//     }
-// }, false);
