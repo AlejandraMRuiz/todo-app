978f72138c111f23cf4ca9cee12b0d26e9a1e408
AlejandraMRuiz <kovydcode@gmail.com>
2020-12-08 21:43:51 -0800

    adds code review points

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ Try it [here!](https://alejandramruiz.github.io/todo-app/)
 As basic as to-do apps are, this one actually represented quite the learning curve for me, and I'm grateful for the lessons. In the making of it, I got a better handle on the use of event listeners, increased my familiarity with using the JavaScript console straight from the browser, discovered new properties and a whole lot more. After much beginner's trial and error, my simple to-do application is one that I'm truly very proud of. Ready to take on the next challenge!  
 
 ## Author 
-* Alejandra M. Ruiz - Full Stack Developer - [Linkedin](https://www.linkedin.com/in/alejandra-m-ruiz/)
+* Alejandra M. Ruiz - Full Stack Developer | [Linkedin](https://www.linkedin.com/in/alejandra-m-ruiz/)
 
--- todo-app.js
+++ todo-app.js
@@ -1,27 +1,13 @@
 
-const inputBox = document.getElementById("inputBox");
-const deleteListBtn = document.getElementById("deleteListBtn");
-const ul = document.getElementById("list");
-const li = document.createElement("li");
-const deleteItemBtn = document.createElement("deleteItemBtn");
-deleteItemBtn.className = "deleteItemBtn";
-li.appendChild(deleteItemBtn);
-const item = li.textContent;
-const text = inputBox.value;
 const savedItems = JSON.parse(localStorage.getItem("userList")) || [];
-
-
-savedItems.forEach((item) => {
-    renderTask(item);
-});    
+savedItems.forEach((item) => renderTask(item));    
 
 
 function renderTask(task)   {
     const ul = document.getElementById("list");
-// isn't const li below unnecessary since it's already created globally?
-// I broke the app on commenting out const li, but that doesn't make sense as it's global...
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(task.text));
+
     const deleteItemBtn = document.createElement("button");
     deleteItemBtn.className = "deleteItemBtn";
     deleteItemBtn.appendChild(document.createTextNode("x"));
@@ -37,6 +23,52 @@ function renderTask(task)   {
         li.style.textDecoration = "line-through";
     }
 
+    checkOffItemAndSaveToLocalStorage(li, task);
+
+    ul.appendChild(li);
+}
+
+
+const inputBox = document.getElementById("inputBox");
+inputBox.addEventListener("keyup", addTask);
+function addTask(event)   {
+    const text = inputBox.value;
+    
+    if (!text)  {
+        alert("Please add a todo")
+    }   else if (event.keyCode === 13)  {
+        const todoItem = {
+            text: inputBox.value,
+            isChecked: false
+        };    
+        renderTask(todoItem);
+
+        inputBox.value = "";
+        inputBox.placeholder = "";
+
+        savedItems.push(todoItem);
+        }
+        localStorage.setItem("userList", JSON.stringify(savedItems));
+}
+
+
+const deleteListBtn = document.getElementById("delete-list-btn");
+deleteListBtn.addEventListener("click", deleteList);
+function deleteList() {
+    document.getElementById("list").remove();
+    localStorage.clear();
+    inputBox.focus();
+}
+
+
+function deleteSingleTaskFromLocalStorage(taskText) {
+    const savedTasks = JSON.parse(localStorage.getItem("userList")) || [];
+    const filteredTasks = savedTasks.filter((task) => task.text !== taskText);
+    localStorage.setItem("userList", JSON.stringify(filteredTasks));
+}
+
+
+function checkOffItemAndSaveToLocalStorage(li, task)    {
     li.addEventListener(
         "click",
         function(e) {
@@ -60,51 +92,8 @@ function renderTask(task)   {
              localStorage.setItem("userList", JSON.stringify(savedItems));
             }
         }, false);
-
-    ul.appendChild(li);
 }
 
-
-function addTask(keyboardEvent)   {
-    if(keyboardEvent.keyCode === 13)   {
-        
-        const todoItem = {
-            text: inputBox.value,
-            isChecked: false
-            }
-        
-        renderTask(todoItem);
-
-        inputBox.value = "";
-        inputBox.placeholder = "";
-
-        savedItems.push(todoItem);
-        localStorage.setItem("userList", JSON.stringify(savedItems));
-    }
-}
-
-
-inputBox.addEventListener("keyup", addTask);
-
-
-function deleteList() {
-    const parent = document.getElementById("list");
-    parent.textContent = "";
-    localStorage.clear();
-    inputBox.focus();
-}
-
-
-function deleteSingleTaskFromLocalStorage(taskText) {
-    const savedTasks = JSON.parse(localStorage.getItem("userList")) || [];
-    const filteredTasks = savedTasks.filter(function(task) {
-        return task.text !== taskText;
-    });
-    localStorage.setItem("userList", JSON.stringify(filteredTasks));
-}
-
-
-deleteListBtn.addEventListener("click", deleteList);
 
 
 
@@ -119,64 +108,6 @@ deleteListBtn.addEventListener("click", deleteList);
 
 
 // PROJECT PLAN:
-
-// STEP 1: Create a user input field which accepts user's value.
-// Don't use this: todo = window.prompt("Add a todo: ");
-// Check functionality using: let inputValue = document.getElementById("enter").value;
-// DONE!!!
-
-// STEP 2: Create a button that when clicked will submit user input to UL.
-// Check button functionality using console.log.
-// Create list items dynamically by using document.createElement("li");
-// Check functionality.
-// DONE!!!
-
-// STEP 3: When task completed, user clicks task to strikethrough. 
-// If user clicks task is true then console.log strikethrough works.
-// DONE!!!
-
-// STEP 4: Add a "delete list" button and get it working.
-// Check it.
-// DONE!!!
-
-// STEP 5: Create a button that individually deletes tasks that are strikethrough.
-// Check button functionality using console.log.
-// DONE!!!
-
-// STEP 6: Fix input field so it clears after hitting enter vs. user doing so manually.
-// Check it.
-// DONE!!!
-
-// STEP 7: Clear placeholder "add a todo" in input element.
-// Check it.
-// DONE!!!
-
-// STEP 8: Get local storage bit working.
-// Check it.
-// DONE!!!
-
-// STEP 9: Ensure that when user clicks task again, the strikethrough comes off it
-// in case they accidentally marked a task as complete.
-// Check it.
-// DONE!!!
-
-// STEP 10: Eliminate extra space left behind when an item mid-list is deleted.
-// Check it.
-// DONE!!!
-
-// STEP 11: Change out any "innerHTMLs" for alternative, textContent.
-// Check it.
-// DONE!!!
-
-// STEP 12: Handle WET code (look into: ul & li global consts + appendChild).
-// Check it.
-// DONE!!!
-
-// STEP 13: Add button to delete individual list items.
-// Check it.
-// DONE!!!
-
-
 
 // STEP 14: Ensure correct use of const & let.
 // Check it.
